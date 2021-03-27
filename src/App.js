6e7e94605b24312e3d49f9b646273648eeef5e71
jesstoh <jesstoh23@gmail.com>
2021-03-27 17:17:32 +0800

    Linking MDBReact UI Kit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import React, {Component} from "react";
+import { MDBBtn } from "mdbreact";
+
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <h1>WiVolunteer</h1>
+        <MDBBtn>Button</MDBBtn>
       </React.Fragment>
     )
   }
