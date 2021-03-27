8185d47e177e2d02be89b0795a897a544140d051
jesstoh <jesstoh23@gmail.com>
2021-03-27 15:41:14 +0800

    Setup create-react-app skeleton

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React, {Component} from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>WiVolunteer</h1>
+      </React.Fragment>
+    )
+  }
+}
+
+export default App;
