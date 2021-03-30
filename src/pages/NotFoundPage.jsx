12dc2feb1a031c785e35954bf1ffb62bb4e47e7c
Wenjie <wenjietj@gmail.com>
2021-03-31 00:04:31 +0800

    page not found component done

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,13 +1,30 @@
-import React, { Component } from "react";
-
+import React, { Component } from 'react';
+import { MDBTypography, MDBBtn } from 'mdbreact';
 class NotFoundPage extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                <h1>404 Front End URL Not Found</h1>
-            </React.Fragment>
-        );
-    }
+	render() {
+		return (
+			<React.Fragment>
+				<MDBTypography className='text-center '>
+					<MDBTypography tag='h1' variant='display-1'>
+						<strong>Oops!</strong>
+					</MDBTypography>
+
+					<MDBTypography tag='h4' colorText='red'>
+						<strong>404:</strong>
+						<small className='text-muted '> Page not found.</small>
+					</MDBTypography>
+
+					<MDBTypography tag='h5' variant='h5-responsive' className='mt-5'>
+						Let's get you back.
+					</MDBTypography>
+
+					<MDBBtn href='/home' color='primary'>
+						Back to home
+					</MDBBtn>
+				</MDBTypography>
+			</React.Fragment>
+		);
+	}
 }
 
 export default NotFoundPage;
