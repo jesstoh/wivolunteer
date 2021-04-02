d1ee8a22c5f086778eb4aade07425464526f86f1
wjtee89 <wenjietj@gmail.com>
2021-04-02 19:50:09 +0800

    page able to fetch user events

--- src/pages/UserEvents.jsx
+++ src/pages/UserEvents.jsx
@@ -1,13 +1,64 @@
-import React, { Component } from "react";
-
+import React, { Component } from 'react';
+import { MDBContainer, MDBTypography, MDBCol, MDBRow } from 'mdbreact';
+import axios from 'axios';
 class UserEvents extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                <h1>User Events Page</h1>
-            </React.Fragment>
-        );
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			userEvents: {},
+		};
+
+		this.onClick = this.onClick.bind(this);
+	}
+
+	onClick(event) {
+		event.preventDefault();
+	}
+
+	componentDidMount() {
+		// get token from local storage
+		const token = localStorage.getItem('token');
+
+		// fetch user events
+		axios
+			.get(`${process.env.REACT_APP_API_URL}/users/events`, {
+				headers: { authorization: `Bearer ${token}` },
+			})
+			.then((response) => {
+				// set user events to state
+				this.setState({ userEvents: response.data });
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+				<MDBContainer className='text-center mt-4 '>
+					<h1>My Events</h1>
+					<MDBRow className='mt-4'>
+						<MDBCol size='4'>
+							<h5 onClick={this.onClick}>
+								<a>Current Events</a>
+							</h5>
+						</MDBCol>
+						<MDBCol size='4'>
+							<h5 onClick={this.onClick}>
+								<a>Leave Feedback</a>
+							</h5>
+						</MDBCol>
+						<MDBCol size='4'>
+							<h5 onClick={this.onClick}>
+								<a>Past Events</a>
+							</h5>
+						</MDBCol>
+					</MDBRow>
+				</MDBContainer>
+			</React.Fragment>
+		);
+	}
 }
 
 export default UserEvents;
