be720e7e08d777a8f2e24119557b2be26c5b96d2
jesstoh <jesstoh23@gmail.com>
2021-04-03 00:06:18 +0800

    Fetch individual event data

--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -1,10 +1,43 @@
+import axios from "axios";
 import React, { Component } from "react";
 
 class Event extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { event: null, redirect: null };
+    }
+
+    fetchEvent() {
+        const token = localStorage.getItem("token");
+        axios
+            .get(
+                `${process.env.REACT_APP_API_URL}/events/${this.props.match.params.id}`,
+                { headers: { authorization: `Bearer ${token}` } }
+            )
+            .then((response) => {
+                this.setState({ event: response.data }, () => {
+                    console.log(this.state.event);
+                });
+            })
+            .catch((err) => {
+                switch (err.response.status) {
+                    case 401:
+                        this.props.handleLogout();
+                        break;
+                    default:
+                        window.location.href = `/404`;
+                }
+            });
+    }
+
+    componentDidMount() {
+        this.fetchEvent();
+    }
+
     render() {
-        return (
+        return !this.state.event ? null : (
             <React.Fragment>
-                <h1>Event Page</h1>
+                <h1>Event Page {this.props.match.params.id}</h1>
             </React.Fragment>
         );
     }
