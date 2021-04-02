0ad8af6bbf63764c38e105a017c146ed57eb2ca6
Wenjie <wenjietj@gmail.com>
2021-04-02 09:11:12 +0800

    fix pagenotfound errors

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
-import { MDBTypography, MDBBtn } from 'mdbreact';
+import { MDBContainer, MDBTypography, MDBBtn } from 'mdbreact';
 class NotFoundPage extends Component {
 	render() {
 		return (
 			<React.Fragment>
-				<MDBTypography className='text-center '>
+				<MDBContainer className='text-center '>
 					<MDBTypography tag='h1' variant='display-1'>
 						<strong>Oops!</strong>
 					</MDBTypography>
 
 					<MDBTypography tag='h4' colorText='red'>
-						<strong>404:</strong>
-						<small className='text-muted '> Page not found.</small>
+						<span>
+							<strong>404:</strong>
+							<small className='text-muted '> Page not found.</small>
+						</span>
 					</MDBTypography>
 
 					<MDBTypography tag='h5' variant='h5-responsive' className='mt-5'>
@@ -21,7 +23,7 @@ class NotFoundPage extends Component {
 					<MDBBtn href='/home' color='primary'>
 						Back to home
 					</MDBBtn>
-				</MDBTypography>
+				</MDBContainer>
 			</React.Fragment>
 		);
 	}
