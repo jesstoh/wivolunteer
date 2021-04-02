25f772c34abfb763c30b06badfe633ce44e16e2d
jesstoh <jesstoh23@gmail.com>
2021-04-02 12:03:10 +0800

    Home page component skeleton

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
+import { MDBContainer, MDBRow, MDBCol, MDBBtn } from "mdbreact";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 class HomePage extends Component {
     render() {
         return (
-            <React.Fragment>
-                <h1>Home Page</h1>
-            </React.Fragment>
+            <MDBContainer className="pt-4">
+                <MDBRow>Search Bar</MDBRow>
+                <MDBRow>
+                    <MDBBtn>Interest</MDBBtn>
+                    <MDBBtn>All</MDBBtn>
+                </MDBRow>
+                <MDBRow>
+                    <MDBCol md="7" lg="8" >Event Display</MDBCol>
+                    <MDBCol md="5" lg="4" >
+                        <Calendar className="mx-auto"/>
+                    </MDBCol>
+                </MDBRow>
+            </MDBContainer>
         );
     }
 }
