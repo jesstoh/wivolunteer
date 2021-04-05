7cb00272ed0f5a42fa6bc143bcf2514515c6149c
jesstoh <jesstoh23@gmail.com>
2021-04-05 23:48:52 +0800

    Add image to event card

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -6,6 +6,7 @@ import {
     MDBCardTitle,
     MDBCardText,
     MDBCol,
+    MDBRow
 } from "mdbreact";
 import { Link } from "react-router-dom";
 import Moment from "react-moment";
@@ -21,6 +22,10 @@ class EventCard extends Component {
                                 {this.props.event.eventTitle}
                             </MDBCardTitle>
                             <MDBCardText>
+                            <MDBRow>
+                                <MDBCol md="4"><img src={this.props.event.image} className="img-fluid img-thumbnail"/>
+                                </MDBCol>
+                                <MDBCol md="8">
                                 Date: <Moment date={this.props.event.dateTime} format="ddd, DD MMM YYYY"/> <br />
                                 Time: <Moment date={this.props.event.dateTime} format="LT"/> <br />
                                 Organizer: {
@@ -32,7 +37,9 @@ class EventCard extends Component {
                                 Participants:{" "}
                                 {this.props.event.participants.length} /{" "}
                                 {this.props.event.limit} Wish:{" "}
-                                {this.props.event.interested.length}
+                                {this.props.event.interested.length}</MDBCol>
+                            </MDBRow>
+                                
                             </MDBCardText>
                         </MDBCardBody>
                     </MDBCard>
