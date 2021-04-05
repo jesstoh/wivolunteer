59a719c8b6b5e014bebc65eb72f626e9292f8cc5
jesstoh <jesstoh23@gmail.com>
2021-04-06 00:28:02 +0800

    Style Event Card Component

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 import {
     MDBCard,
     MDBCardBody,
-    MDBCardImage,
+    MDBIcon,
     MDBCardTitle,
     MDBCardText,
     MDBCol,
-    MDBRow
+    MDBRow,
 } from "mdbreact";
 import { Link } from "react-router-dom";
 import Moment from "react-moment";
@@ -14,33 +14,56 @@ import Moment from "react-moment";
 class EventCard extends Component {
     render() {
         return (
-            <MDBCol>
+            <MDBCol className="mb-1">
                 <Link to={"/event/" + this.props.event._id}>
                     <MDBCard style={{ width: "90%" }}>
                         <MDBCardBody>
                             <MDBCardTitle>
                                 {this.props.event.eventTitle}
                             </MDBCardTitle>
-                            <MDBCardText>
                             <MDBRow>
-                                <MDBCol md="4"><img src={this.props.event.image} className="img-fluid img-thumbnail"/>
+                                <MDBCol md="4">
+                                    <img
+                                        src={this.props.event.image}
+                                        className="img-fluid img-thumbnail"
+                                        style={{ "max-height": "7rem" }}
+                                    />
                                 </MDBCol>
-                                <MDBCol md="8">
-                                Date: <Moment date={this.props.event.dateTime} format="ddd, DD MMM YYYY"/> <br />
-                                Time: <Moment date={this.props.event.dateTime} format="LT"/> <br />
-                                Organizer: {
-                                    this.props.event.organiser.username
-                                }{" "}
-                                <br />
-                                Location: {this.props.event.location} <br />
-                                Type: {this.props.event.eventType.join(", ")} <br />
-                                Participants:{" "}
-                                {this.props.event.participants.length} /{" "}
-                                {this.props.event.limit} Wish:{" "}
-                                {this.props.event.interested.length}</MDBCol>
+                                <MDBCol md="6">
+                                    <MDBCardText>
+                                        <MDBIcon far icon="calendar-alt" />{" "}
+                                        <Moment
+                                            date={this.props.event.dateTime}
+                                            format="ddd, DD MMM YYYY"
+                                        />{" "}
+                                        <br />
+                                        <MDBIcon far icon="clock" />{" "}
+                                        <Moment
+                                            date={this.props.event.dateTime}
+                                            format="LT"
+                                        />{" "}
+                                        <br />
+                                        <MDBIcon icon="map-marker-alt" />{" "}
+                                        {this.props.event.location} <br />
+                                        Organizer:{" "}
+                                        {this.props.event.organiser.username}
+                                        <br />
+                                        Type:{" "}
+                                        {this.props.event.eventType.join(
+                                            ", "
+                                        )}{" "}
+                                        <br />
+                                        Participants:{" "}
+                                        {
+                                            this.props.event.participants.length
+                                        } / {this.props.event.limit}{" "}
+                                        <span className="pl-3">
+                                            Wish:{" "}
+                                            {this.props.event.interested.length}
+                                        </span>
+                                    </MDBCardText>
+                                </MDBCol>
                             </MDBRow>
-                                
-                            </MDBCardText>
                         </MDBCardBody>
                     </MDBCard>
                 </Link>
