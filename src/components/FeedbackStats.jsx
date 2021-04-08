eb162525ad74dbe4470df915fc7a84ce137f94a1
Wenjie <wenjietj@gmail.com>
2021-04-08 21:31:42 +0800

    chart skeleton

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -3,52 +3,78 @@ import { MDBContainer } from "mdbreact";
 import { Pie, Bar } from "react-chartjs-2";
 
 class FeedbackStats extends Component {
+	render() {
+		const chartOptions = {
+			scales: {
+				yAxes: [
+					{
+						ticks: {
+							beginAtZero: true,
+						},
+					},
+				],
+			},
+		};
+		const chartColors = {
+			pieColors: ["#BA274A", "#3ED388", "#2191FB"],
+			hoverPieColors: ["#FF6384", "#60F2A8", "#5FB9F7"],
 
-    render() {
-        const data = {
-            labels: ["Just Nice", "More than enough ", "Not enough"],
-            datasets: [
-                {
-                    data: [65, 59, 80],
-                    backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
-                    hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
-                },
-            ],
-        };
+			barColors: [
+				"rgb(199, 0, 57)",
+				"rgb(255, 87, 51)",
+				"rgb(255, 195, 0)",
+				"rgb(24, 203, 157)",
+				"rgb(23, 203, 11)",
+			],
+			hoverBarColors: [
+				"rgba(199, 0, 57,0.5)",
+				"rgba(255, 87, 51,0.5)",
+				"rgba(255, 195, 0,0.5)",
+				"rgba(24, 203, 157,0.5)",
+				"rgba(23, 203, 11,0.5)",
+			],
+		};
+		const resourcesFeedback = {
+			labels: ["Not enough", "Just Nice", "More than enough "],
+			datasets: [
+				{
+					data: [65, 59, 80],
+					backgroundColor: chartColors.pieColors, //green //blue //red
+					hoverBackgroundColor: chartColors.hoverPieColors, //green //blue //red
+				},
+			],
+		};
 
-        const data1 = {
-            labels: ['1', '2', '3', '4', '5'],
-            datasets: [
-              {
-                label: 'My First dataset',
-                backgroundColor: 'rgba(255,99,132,0.2)',
-                borderColor: 'rgba(255,99,132,1)',
-                borderWidth: 1,
-                hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-                hoverBorderColor: 'rgba(255,99,132,1)',
-                data: [5, 7, 8, 10, 2]
-              }
-            ]
-          };
-        return (
-            <MDBContainer>
-                <h1>Event statistics</h1>
-                <h2>Did you allocated sufficient resources for your event?</h2>
-                <Pie data={data} />
-                <h2>How well organized is your event?</h2>
-                <Bar
-                    data={data1}
-                    width={100}
-                    height={50}
-                    
-                />
-                <h2>How satisfied are participants after your event?</h2>
+		const data1 = {
+			labels: ["1", "2", "3", "4", "5"],
+			datasets: [
+				{
+					label: ["1", "2", "3", "4", "5"],
+					backgroundColor: chartColors.barColors,
+					hoverBackgroundColor: chartColors.hoverBarColors,
+					data: [5, 5, 7, 8, 10],
+				},
+			],
+		};
+		return (
+			<MDBContainer>
+				<h5>Event statistics</h5>
+				<p>Did you allocated sufficient resources for your event?</p>
 
-                <h2>How satisfied are participants after your event?</h2>
-                <h1>Additional Information</h1>
-            </MDBContainer>
-        );
-    }
+				<Pie data={resourcesFeedback} />
+				<br />
+				<p>How well organized is your event?</p>
+				<Bar data={data1} width={100} height={50} options={chartOptions} />
+				<br />
+				<p>How satisfied are participants after your event?</p>
+				<Bar data={data1} width={100} height={50} options={chartOptions} />
+				<br />
+				<h5>Additional Information</h5>
+				<p>- {"data here"} have submitted feedback for your event.</p>
+				<p>- {"data here"} have participated in your event.</p>
+			</MDBContainer>
+		);
+	}
 }
 
 export default FeedbackStats;
