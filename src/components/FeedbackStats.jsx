d41d1d751de35dccb38bc9bde84c3c13ccd7ddbf
jesstoh <jesstoh23@gmail.com>
2021-04-08 21:30:13 +0800

    Fetch and process feedback data

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,8 +1,50 @@
 import React, { Component } from "react";
 import { MDBContainer } from "mdbreact";
 import { Pie, Bar } from "react-chartjs-2";
+import axios from "axios";
 
 class FeedbackStats extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            qn1: null,
+            qn2: null,
+            qn3: null,
+        };
+    }
+    componentDidMount() {
+        const token = localStorage.getItem("token");
+        axios
+            .get(
+                `${process.env.REACT_APP_API_URL}/feedback/${this.props.eventId}`,
+                { headers: { authorization: `Bearer ${token}` } }
+            )
+            .then((response) => {
+                if (!response.data.length) {
+                    // PROCESS DATA INTO ARRAY///
+                    // create array of 0 value corresponding to number of options in each question
+                    const qn1 = new Array(3).fill(0);
+                    const qn2 = new Array(5).fill(0);
+                    const qn3 = new Array(5).fill(0);
+                    response.data.forEach((feedback) => {
+                        // For question, will change schema to Number and corresponding to string, to make it easier to process
+                        if (feedback.hasEnoughResources === "Not enough") {
+                            qn1[0]++;
+                        } else if (
+                            feedback.hasEnoughResources === "Just nice"
+                        ) {
+                            qn1[1]++;
+                        } else {
+                            qn1[2]++;
+                        }
+                        qn2[feedback.isWellOrganised - 1]++; // Adding count to each question array
+                        qn3[feedback.isSatisfied - 1]++;
+                    });
+                    this.setState({ qn1: qn1, qn2: qn2, qn3: qn3 });
+                    // console.log(qn1, qn2, qn3);
+                }
+            });
+    }
 
     render() {
         const data = {
@@ -17,19 +59,31 @@ class FeedbackStats extends Component {
         };
 
         const data1 = {
-            labels: ['1', '2', '3', '4', '5'],
+            labels: ["1", "2", "3", "4", "5"],
             datasets: [
-              {
-                label: 'My First dataset',
-                backgroundColor: 'rgba(255,99,132,0.2)',
-                borderColor: 'rgba(255,99,132,1)',
-                borderWidth: 1,
-                hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-                hoverBorderColor: 'rgba(255,99,132,1)',
-                data: [5, 7, 8, 10, 2]
-              }
-            ]
-          };
+                {
+                    label: "My First dataset",
+                    backgroundColor: "rgba(255,99,132,0.2)",
+                    borderColor: "rgba(255,99,132,1)",
+                    borderWidth: 1,
+                    hoverBackgroundColor: "rgba(255,99,132,0.4)",
+                    hoverBorderColor: "rgba(255,99,132,1)",
+                    data: [5, 7, 8, 10, 8],
+                },
+            ],
+        };
+
+        const chartOptions = {
+            scales: {
+                yAxes: [
+                    {
+                        ticks: {
+                            beginAtZero: true,
+                        },
+                    },
+                ],
+            },
+        };
         return (
             <MDBContainer>
                 <h1>Event statistics</h1>
@@ -40,7 +94,7 @@ class FeedbackStats extends Component {
                     data={data1}
                     width={100}
                     height={50}
-                    
+                    options={chartOptions}
                 />
                 <h2>How satisfied are participants after your event?</h2>
 
