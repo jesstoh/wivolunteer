e3fe49c84141fec5951745694ef36a1c7faacaae
Wenjie <wenjietj@gmail.com>
2021-04-09 00:07:38 +0800

    profile page styling

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -3,6 +3,10 @@ import {
 	MDBContainer,
 	MDBRow,
 	MDBCol,
+	MDBCard,
+	MDBCardTitle,
+	MDBCardBody,
+	MDBBCardText,
 	MDBBadge,
 	MDBBtn,
 	MDBIcon,
@@ -56,41 +60,58 @@ class UserProfile extends Component {
 							/>
 						</MDBCol>
 					</MDBRow>
-					<h5>
-						<MDBIcon icon="user" className="mr-3" />
-						{this.state.userProfile.username}
-					</h5>
-					<h5>
-						<MDBIcon icon="envelope" className="mr-3" />
-						{this.state.userProfile.email}
+					<MDBRow className="mb-4">
+						<MDBCol>
+							<h3 className=" font-weight-bolder">
+								{this.state.userProfile.username}
+							</h3>
+						</MDBCol>
+					</MDBRow>
+					<MDBCard>
+						<MDBCardBody>
+							<MDBRow>
+								<MDBCol xl="12" className="mt-3 ">
+									<h5 className="mb-4 text-center">
+										<MDBIcon icon="envelope" className="mr-2" />
+										{this.state.userProfile.email}
+									</h5>
 
-						<MDBIcon icon="phone" className="mr-2 ml-2" />
-						{this.state.userProfile.contact}
-					</h5>
+									<h5>
+										<MDBIcon icon="phone" className="mr-2 ml-2" />
+										{this.state.userProfile.contact}
+									</h5>
 
-					<h5>
-						<MDBIcon icon="map-marked" className="mr-3" />
-						{this.state.userProfile.address}
-					</h5>
-					<h5>
-						<span>Interests: </span>
-						{!this.state.userProfile
-							? ""
-							: this.state.userProfile.interests.map((interest) => {
-									return (
-										<MDBBadge
-											key={interest}
-											pill
-											color="primary"
-											className="m-2"
-										>
-											{interest}
-										</MDBBadge>
-									);
-							  })}
-					</h5>
+									<h5>
+										<MDBIcon icon="map-marked" className="mr-3" />
+										{this.state.userProfile.address}
+									</h5>
+								</MDBCol>
+							</MDBRow>
+							<MDBRow>
+								<MDBCol xl="12" className="mt-3">
+									<h5>
+										<MDBIcon icon="heart" />
+										{!this.state.userProfile
+											? ""
+											: this.state.userProfile.interests.map((interest) => {
+													return (
+														<MDBBadge
+															key={interest}
+															pill
+															color="primary"
+															className="m-2"
+														>
+															{interest}
+														</MDBBadge>
+													);
+											  })}
+									</h5>
+								</MDBCol>
+							</MDBRow>
+						</MDBCardBody>
+					</MDBCard>
 					<Link to="/profile/edit">
-						<MDBBtn color="blue" size="md" type="submit" className="mb-4">
+						<MDBBtn color="blue" type="submit" className="mb-4 mt-5">
 							Edit Profile
 						</MDBBtn>
 					</Link>
